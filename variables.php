36a858d82e79f505ce77492c67819975de281f2f
Alfred <alfred.-dom@hotmail.com>
2017-10-13 15:28:57 +0200

    se agrego el trabajo con variables

---
+++ variables.php
@@ -0,0 +1,23 @@
+			<!DOCTYPE html>
+<html lang ="en">
+<head>
+
+
+
+</head>
+
+<body>
+	<?php 
+	echo "<h1>mi primer script php </h1>";
+		$x = 5 + 5 ;
+		$y =10;
+		$w =$x+y;
+		echo $w;
+		$texto= "hola mundo";
+		echo "<h2>$texto</h2>";
+		$booleano=true;
+		echo "<h2>".$texto. " ".$booleano."</h2>";
+		$z=5;
+?>
+</body>
+</html>
